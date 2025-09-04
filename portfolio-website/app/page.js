e3d95091ab351d529b5310cba70b9dc9f9ad56fa
Ariston Zubiri <aristontheanalyst@gmail.com>
2025-09-04 22:45:16 +1200

    added comments for clarity

--- portfolio-website/app/page.js
+++ portfolio-website/app/page.js
@@ -2,6 +2,9 @@ import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import NavBar from "./components/NavBar";
 
+// This is the main page component, which will include multiple components that go from top to bottom
+// execute npm run dev to see the changes on localhost:3000
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
