f91e1a09bea5c81f07a0511d6c44002d98cb2645
Ariston Zubiri <aristontheanalyst@gmail.com>
2025-09-06 22:37:10 +1200

    began building about section

---
+++ portfolio-website/app/components/AboutSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutSection = () => {
+  return (
+    <section className="text-white">
+      <div>AboutSection</div>
+    </section>
+  )
+}
+
+export default AboutSection
--- portfolio-website/app/page.js
+++ portfolio-website/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import NavBar from "./components/NavBar";
+import AboutSection from "./components/AboutSection";
 
 // This is the main page component, which will include multiple components that go from top to bottom
 // execute npm run dev to see the changes on localhost:3000
@@ -11,6 +12,7 @@ export default function Home() {
       <NavBar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
+        <AboutSection />
       </div>
     </main>
   );
